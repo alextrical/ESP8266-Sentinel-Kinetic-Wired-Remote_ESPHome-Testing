af4e090a1f46c59602d31f9b7675266af7211d56
alextrical <35117191+alextrical@users.noreply.github.com>
2025-05-23 12:00:29 +0100

    Repo cleanup

--- config/components/ventaxiask_uart/button/query_button.cpp
+++
@@ -1,9 +0,0 @@
-#include "query_button.h"
-
-namespace esphome {
-namespace ventaxiask_uart {
-
-void QueryButton::press_action() { this->parent_->read_all_info(); }
-
-}  // namespace ventaxiask_uart
-}  // namespace esphome
