9d7ae67f2e5151a66212bd54476c8752f42793b7
alextrical <35117191+alextrical@users.noreply.github.com>
2025-05-24 15:22:47 +0100

    Removed mac_address

--- config/components/vent_axia_sentinel_kinetic/text_sensor.py
+++ config/components/vent_axia_sentinel_kinetic/text_sensor.py
@@ -2,7 +2,6 @@ import esphome.codegen as cg
 from esphome.components import text_sensor
 import esphome.config_validation as cv
 from esphome.const import (
-    CONF_MAC_ADDRESS,
     CONF_VERSION,
     ENTITY_CATEGORY_DIAGNOSTIC,
     ICON_BLUETOOTH,
@@ -18,9 +17,6 @@ CONFIG_SCHEMA = {
     cv.Optional(CONF_VERSION): text_sensor.text_sensor_schema(
         entity_category=ENTITY_CATEGORY_DIAGNOSTIC, icon=ICON_CHIP
     ),
-    cv.Optional(CONF_MAC_ADDRESS): text_sensor.text_sensor_schema(
-        entity_category=ENTITY_CATEGORY_DIAGNOSTIC, icon=ICON_BLUETOOTH
-    ),
 }
 
 
@@ -29,6 +25,3 @@ async def to_code(config):
     if version_config := config.get(CONF_VERSION):
         sens = await text_sensor.new_text_sensor(version_config)
         cg.add(vent_axia_sentinel_kinetic_component.set_version_text_sensor(sens))
-    if mac_address_config := config.get(CONF_MAC_ADDRESS):
-        sens = await text_sensor.new_text_sensor(mac_address_config)
-        cg.add(vent_axia_sentinel_kinetic_component.set_mac_text_sensor(sens))
