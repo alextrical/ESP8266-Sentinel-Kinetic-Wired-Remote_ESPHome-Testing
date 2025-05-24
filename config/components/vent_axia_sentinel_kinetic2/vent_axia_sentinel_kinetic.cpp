760b64a5f3c9ae7e5a0684b3e96e3bacf0e26d4e
alextrical <35117191+alextrical@users.noreply.github.com>
2025-05-24 09:44:13 +0100

    checkin

---
+++ config/components/vent_axia_sentinel_kinetic2/vent_axia_sentinel_kinetic.cpp
@@ -0,0 +1,62 @@
+#include "esphome/core/log.h"
+#include "vent_axia_sentinel_kinetic.h"
+
+#define highbyte(val) (uint8_t)((val) >> 8)
+#define lowbyte(val) (uint8_t)((val) & 0xff)
+
+namespace esphome {
+namespace vent_axia_sentinel_kinetic {
+
+static const char *TAG = "vent_axia_sentinel_kinetic.component";
+
+void VentAxiaSentinelKineticComponent::setup() {
+  ESP_LOGCONFIG(TAG, "Setting up VentAxiaSentinelKinetic...");
+  this->send_alive_str_();
+//   ESP_LOGCONFIG(TAG, "Mac Address : %s", const_cast<char *>(this->mac_.c_str()));
+//   ESP_LOGCONFIG(TAG, "Firmware Version : %s", const_cast<char *>(this->version_.c_str()));
+  ESP_LOGCONFIG(TAG, "VentAxiaSentinelKinetic setup complete.");
+}
+
+void VentAxiaSentinelKineticComponent::loop() {
+
+}
+
+void VentAxiaSentinelKineticComponent::dump_config(){
+  ESP_LOGCONFIG(TAG, "VentAxiaSentinelKinetic:");
+#ifdef USE_BUTTON
+  // LOG_BUTTON("  ", "UpButton", this->up_button_);
+#endif
+}
+
+void VentAxiaSentinelKineticComponent::send_alive_str_() { this->send_command_(CMD_ALIVE_HEADER, 4, CMD_ALIVE_DATA); }
+
+void VentAxiaSentinelKineticComponent::send_command_(const uint8_t *command_value, int command_value_len, uint8_t command) {
+  ESP_LOGV(TAG, "Sending COMMAND %02X", command);
+
+  // frame start bytes
+  this->write_byte(CMD_FRAME_HEADER);
+  uint16_t crc = 0xFFFF - CMD_FRAME_HEADER;
+
+  // command value bytes
+  if (command_value != nullptr) {
+    for (int i = 0; i < command_value_len; i++) {
+      this->write_byte(command_value[i]);
+      crc -= command_value[i];
+    }
+  }
+
+  // command
+  this->write_byte(command);
+  crc -= command;
+
+  // frame end bytes
+  //Write CRC
+  this->write_byte(lowbyte(crc));
+  this->write_byte(highbyte(crc));
+  
+  // FIXME to remove
+  // delay(50);  // NOLINT
+}
+
+}  // namespace vent_axia_sentinel_kinetic
+}  // namespace esphome
---
+++ config/components/vent_axia_sentinel_kinetic2/vent_axia_sentinel_kinetic.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/uart/uart.h"
+
+namespace esphome {
+namespace vent_axia_sentinel_kinetic {
+
+// Commands
+static const uint8_t CMD_FRAME_HEADER = 0x04;
+static const uint8_t CMD_ALIVE_HEADER[4] = {0x06, 0xFF, 0xFF, 0xFF};
+static const uint8_t CMD_KEY_HEADER[4] = {0x05, 0xAF, 0xEF, 0xFB};
+static const uint8_t CMD_ALIVE_DATA = 0x10;
+
+class VentAxiaSentinelKineticComponent : public uart::UARTDevice, public Component {
+  public:
+    void setup() override;
+    void loop() override;
+    void dump_config() override;
+
+  protected:
+    void send_command_(const uint8_t *command_value, int command_value_len, uint8_t command_str);
+    void send_alive_str_();
+};
+
+
+}  // namespace vent_axia_sentinel_kinetic
+}  // namespace esphome
